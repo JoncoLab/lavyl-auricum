afe60735a19108eee66d4923adffcaed8a89f224
Sher <max.pechko@gmail.com>
2017-07-29 21:05:19 +0300

    0.2

--- src/scripts/js/script.js
+++ src/scripts/js/script.js
@@ -13,6 +13,37 @@ var main = function () {
         bigAmountSens = $('#big-item-sens');
 
     smallAmount.addClass('current');
+    smallAmountSens.addClass('current');
+    bigIt.hide();
+    bigItSens.hide();
+
+    bigAmount.click(function () {
+        smallAmount.removeClass('current');
+        $(this).addClass('current');
+        smallIt.hide();
+        bigIt.fadeIn('slow');
+    });
+
+    smallAmount.click(function () {
+        bigAmount.removeClass('current');
+        $(this).addClass('current');
+        bigIt.hide();
+        smallIt.fadeIn('slow');
+    });
+
+    bigAmountSens.click(function () {
+        smallAmountSens.removeClass('current');
+        $(this).addClass('current');
+        smallItSens.hide();
+        bigItSens.fadeIn('slow');
+    });
+
+    smallAmountSens.click(function () {
+        bigAmountSens.removeClass('current');
+        $(this).addClass('current');
+        bigItSens.hide();
+        smallItSens.fadeIn('slow');
+    });
 };
 
 $(document).ready(main);
